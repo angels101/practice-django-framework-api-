6d3a86e34fbaacf19080f341cf7665c2285cc629
angels101 <angelscodex101@gmail.com>
2021-04-13 21:04:41 +0300

    templates/registaration base-login and registration form

--- evaluate/settings.py
+++ evaluate/settings.py
@@ -97,7 +97,7 @@ ROOT_URLCONF = 'evaluate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
