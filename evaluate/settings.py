d7ca957d7a1025a9cf1edefeaa958f560bd37d16
angels101 <angelscodex101@gmail.com>
2021-04-13 09:27:02 +0300

    license,makefile,Procfile,Readme &runtime,requirements

--- evaluate/settings.py
+++ evaluate/settings.py
@@ -69,7 +69,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'awards',
     
+
 ]
 
 MIDDLEWARE = [
