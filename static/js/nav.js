c01d9ad762041f652d55ee4b04f5e560cf895272
angels101 <angelscodex101@gmail.com>
2021-04-13 16:43:44 +0300

    static

---
+++ static/js/nav.js
@@ -0,0 +1,12 @@
+$(window).scroll(() => {
+    if ($(document).scrollTop() > 450) {
+        $("nav").addClass("shrink")
+        $(".navbar-form").show()
+        
+    } else  {
+        $("nav").removeClass("shrink")
+        $(".navbar-form").hide()
+
+    }
+})
+
