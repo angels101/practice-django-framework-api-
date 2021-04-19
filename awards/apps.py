6298a5499de00aa22e54f8a1674b50dc2719ca08
angels101 <angelscodex101@gmail.com>
2021-04-19 14:10:02 +0300

    make it better

--- awards/apps.py
+++ awards/apps.py
@@ -11,8 +11,7 @@ class AwardsConfig(AppConfig):
 
 # foo/apps.py
 
-from django.apps import AppConfig
 
-class FooConfig(AppConfig):
-    name = 'full.python.path.to.your.app.foo'
-    label = 'my.foo'  # <-- this is the important line - change it to anything other than the default, which is the module name ('foo' in this case)
+#class FooConfig(AppConfig):
+   # name = 'full.python.path.to.your.app.foo'
+    #label = 'my.foo'  # <-- this is the important line - change it to anything other than the default, which is the module name ('foo' in this case)
