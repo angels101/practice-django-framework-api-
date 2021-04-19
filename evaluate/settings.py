15ff337a883b19ed585c9546a165c80fab1c2d7f
angels101 <angelscodex101@gmail.com>
2021-04-19 12:22:40 +0300

    make it better

--- evaluate/settings.py
+++ evaluate/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 
 
 #import environ
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #load_dotenv(os.path.join(BASE_DIR, '.env'))
 #env = environ.Env(
@@ -74,7 +74,7 @@ else:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lol@)@7=srza_6!6-e-y#f#@z!$eg1du9y4mbrjcr^qu(vjfy0'
+#SECRET_KEY = 'django-insecure-lol@)@7=srza_6!6-e-y#f#@z!$eg1du9y4mbrjcr^qu(vjfy0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
